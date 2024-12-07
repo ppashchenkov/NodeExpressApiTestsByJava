a2494ebc5d186647d73c42b49cc9ef60d24f6c01
ppashchenkov <ppashchenkov@gmail.com>
2024-12-07 16:32:49 +0300

    Refactoring

--- src/test/java/node_express_api/utils/TestData.java
+++ src/test/java/node_express_api/utils/TestData.java
@@ -3,30 +3,36 @@ package node_express_api.utils;
 public class TestData {
     public static final String BASE_URL = "http://localhost:5000";
     public static final String HOME_END_POINT = "/";
-    public static final String H1_CONTENT = "Node Express API Server App";
-    public static final String ADD_TITLE = "Users app";
-    public static final String SEARCH_TITLE = "Search user";
-    public static final String EDIT_TITLE = "Edit user";
-    public static final String DELETE_TITLE = "Delete user";
-    public static final String H2_ADD_TITLE = "Add User";
-    public static final String H2_SEARCH_TITLE = "Search user";
-    public static final String H2_EDIT_TITLE = "Edit user";
-    public static final String H2_DELETE_TITLE = "Delete user";
-    public static final String ADD_MENU = "Add";
-    public static final String SEARCH_MENU = "Search";
-    public static final String EDIT_MENU = "Edit";
-    public static final String DELETE_MENU = "Delete";
     public static final String ADD_END_POINT = "add";
     public static final String SEARCH_END_POINT = "search";
     public static final String EDIT_END_POINT = "edit";
     public static final String DELETE_END_POINT = "delete";
+
+    public static final String ADD_TITLE = "Users app";
+    public static final String SEARCH_TITLE = "Search user";
+    public static final String EDIT_TITLE = "Edit user";
+    public static final String DELETE_TITLE = "Delete user";
+
+    public static final String H1_CONTENT = "Node Express API Server App";
+    public static final String H2_ADD_TITLE = "Add User";
+    public static final String H2_SEARCH_TITLE = "Search user";
+    public static final String H2_EDIT_TITLE = "Edit user";
+    public static final String H2_DELETE_TITLE = "Delete user";
+
+    public static final String ADD_MENU = "Add";
+    public static final String SEARCH_MENU = "Search";
+    public static final String EDIT_MENU = "Edit";
+    public static final String DELETE_MENU = "Delete";
+
     public static final String LABEL_FIRST_NAME = "First Name";
     public static final String LABEL_LAST_NAME = "Last Name";
     public static final String LABEL_AGE = "Age";
     public static final String LABEL_USER_ID = "User ID";
+
     public static final String FIRST_NAME = "John";
     public static final String LAST_NAME = "Donahue";
     public static final String AGE = "37";
+
     public static final String BUTTON_ADD = "Add";
     public static final String BUTTON_DELETE = "Delete";
 
--- src/test/java/node_express_api/utils/UserData.java
+++ src/test/java/node_express_api/utils/UserData.java
@@ -2,9 +2,23 @@ package node_express_api.utils;
 
 public class UserData {
     public static class User1 {
-        public static final String firstName = "Jonny";
-        public static final String lastName = "Donahue";
-        public static final String age = "37";
+        public static final String firstName = "John";
+        public static final String lastName = "Doe";
+        public static final String age = "46";
     }
-
+    public static class User2 {
+        public static final String firstName = "Jane";
+        public static final String lastName = "Doe";
+        public static final String age = "35";
+    }
+    public static class User3 {
+        public static final String firstName = "Johnny";
+        public static final String lastName = "Doe";
+        public static final String age = "10";
+    }
+    public static class User4 {
+        public static final String firstName = "Jane";
+        public static final String lastName = "Unique";
+        public static final String age = "46";
+    }
 }
