a5dd9b814f90a55e239d3956bf2e4f9978c2df1f
ppashchenkov <email@example.com>
2024-12-13 11:02:10 +0300

    IconsUiTest.java Done!

--- src/test/java/node_express_api/IconsUiTest.java
+++ src/test/java/node_express_api/IconsUiTest.java
@@ -5,22 +5,16 @@ import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
 import node_express_api.runner.BaseTest;
 import node_express_api.utils.LoggerUtils;
-import node_express_api.utils.User;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 import static node_express_api.utils.TestData.*;
 
 public class IconsUiTest extends BaseTest {
 
     @Test
     public void testIconsUi() {
-        List<Locator> tableRows = new ArrayList<Locator>();
-
+        LoggerUtils.logSuccess("TestCase: testIconsUI");
         getPage().getByRole(AriaRole.LINK,
                 new Page.GetByRoleOptions().setName(ADD_MENU).setExact(true)).click();
         getPage().getByLabel(LABEL_FIRST_NAME).fill(user1.getFirstName());
@@ -28,23 +22,32 @@ public class IconsUiTest extends BaseTest {
         getPage().getByLabel(LABEL_AGE).fill(user1.getAge());
         getPage().getByRole(AriaRole.BUTTON,
                 new Page.GetByRoleOptions().setName(BUTTON_ADD).setExact(true)).click();
-        String actualFirstName = getPage().locator( "tbody>tr>td").first().innerText();
-        String actualLastName = getPage().locator( "tbody>tr>td").nth(1).innerText();
-        String actualAge = getPage().locator( "tbody>tr>td").nth(2).innerText();
         String id = getPage().locator( "tbody>tr>td").nth(3).innerText();
         user1.setId(id);
+        Locator tableRows = getPage().locator( "tbody>tr");
 
-        tableRows = getPage().locator( "tbody>tr").all();
+        assertThat(tableRows).hasCount(1);
 
-//        LoggerUtils.logInfo("+++++++++++++++++++++++++++++++++++++++++++++++++++++++");
-//        LoggerUtils.logInfo(tableRows.toString());
+        Locator userRow = getPage().locator( "table>tbody>tr");
+        Locator userCells = userRow.getByRole(AriaRole.CELL);
+        Locator editIconInUserRow = getPage().locator( "tbody>tr>td").nth(4)
+                .locator("i>a>svg.bi-pen");
+        Locator deleteIconInUserRow = getPage().locator( "tbody>tr>td").nth(5)
+                .locator("i>a>svg.bi-trash");
 
-        Assert.assertEquals(tableRows.size(), 1);
+        assertThat(userRow).hasCount(1);
+        assertThat(userCells).hasCount(6);
+        assertThat(editIconInUserRow).isVisible();
+        assertThat(editIconInUserRow).hasCount(1);
+        assertThat(deleteIconInUserRow).isVisible();
+        assertThat(deleteIconInUserRow).hasCount(1);
 
+        editIconInUserRow.hover( new Locator.HoverOptions().setForce(true));
+
+        assertThat(editIconInUserRow).hasCSS("color", "rgb(0, 128, 0)");
+
+        deleteIconInUserRow.hover( new Locator.HoverOptions().setForce(true));
+
+        assertThat(deleteIconInUserRow).hasCSS("color", "rgb(255, 0, 0)");
     }
-
-
-
-
-
 }
